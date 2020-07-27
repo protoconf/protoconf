45e59b812cc6aea78f8d022a40f73be4424ac7b5
Shahar Mintz <smintz@wix.com>
2020-07-27 09:15:07 +0300

    tests: adds more compiler tests

--- compiler/lib/compiler_test.go
+++ compiler/lib/compiler_test.go
@@ -10,6 +10,9 @@ func Test(t *testing.T) {
 	c := NewCompiler("testdata", true)
 	c.DisableWriting()
 	assert.NoError(t, c.CompileFile("test.pconf"))
+	assert.Error(t, c.CompileFile("validator_test.pconf"))
 	assert.NoError(t, c.CompileFile("enum_test.pconf"))
 	assert.Error(t, c.CompileFile("enum_wrong_types_test.pconf"))
+	assert.NoError(t, c.CompileFile("multioutputs_test.mpconf"))
+	assert.NoError(t, c.CompileFile("include_pinc_test.pconf"))
 }
