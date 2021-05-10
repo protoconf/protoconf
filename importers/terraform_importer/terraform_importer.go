3ec92c8d6b7970fae957e996f9aaafe7a524dc55
Shahar Mintz <smintz@users.noreply.github.com>
2021-05-10 13:37:52 +0300

    fix(terraform): required_providers json name (#80)

--- importers/terraform_importer/terraform_importer.go
+++ importers/terraform_importer/terraform_importer.go
@@ -101,7 +101,7 @@ func addTerraformLocalBackend(b *builder.MessageBuilder, oof *builder.OneOfBuild
 }
 
 func defaultStringField(m *builder.MessageBuilder, name string, comments ...string) {
-	c := builder.Comments{LeadingDetachedComments: comments,}
+	c := builder.Comments{LeadingDetachedComments: comments}
 	m.AddField(builder.NewField(name, builder.FieldTypeString()).SetJsonName(name).SetComments(c))
 }
 
@@ -110,38 +110,38 @@ func addTerraformS3Backend(b *builder.MessageBuilder, oof *builder.OneOfBuilder)
 	defaultStringField(l, "region")
 	defaultStringField(l, "access_key")
 	defaultStringField(l, "secret_key")
-	defaultStringField(l, "iam_endpoint") // (Optional) Custom endpoint for the AWS Identity and Access Management (IAM) API. This can also be sourced from the AWS_IAM_ENDPOINT environment variable.
-	defaultStringField(l, "max_retries") // (Optional) The maximum number of times an AWS API request is retried on retryable failure. Defaults to 5.
-	defaultStringField(l, "profile") // (Optional) Name of AWS profile in AWS shared credentials file (e.g. ~/.aws/credentials) or AWS shared configuration file (e.g. ~/.aws/config) to use for credentials and/or configuration. This can also be sourced from the AWS_PROFILE environment variable.
-	defaultStringField(l, "shared_credentials_file") // (Optional) Path to the AWS shared credentials file. Defaults to ~/.aws/credentials.
+	defaultStringField(l, "iam_endpoint")                // (Optional) Custom endpoint for the AWS Identity and Access Management (IAM) API. This can also be sourced from the AWS_IAM_ENDPOINT environment variable.
+	defaultStringField(l, "max_retries")                 // (Optional) The maximum number of times an AWS API request is retried on retryable failure. Defaults to 5.
+	defaultStringField(l, "profile")                     // (Optional) Name of AWS profile in AWS shared credentials file (e.g. ~/.aws/credentials) or AWS shared configuration file (e.g. ~/.aws/config) to use for credentials and/or configuration. This can also be sourced from the AWS_PROFILE environment variable.
+	defaultStringField(l, "shared_credentials_file")     // (Optional) Path to the AWS shared credentials file. Defaults to ~/.aws/credentials.
 	defaultStringField(l, "skip_credentials_validation") // (Optional) Skip credentials validation via the STS API.
-	defaultStringField(l, "skip_region_validation") // (Optional) Skip validation of provided region name.
-	defaultStringField(l, "skip_metadata_api_check") // (Optional) Skip usage of EC2 Metadata API.
-	defaultStringField(l, "sts_endpoint") // (Optional) Custom endpoint for the AWS Security Token Service (STS) API. This can also be sourced from the AWS_STS_ENDPOINT environment variable.
-	defaultStringField(l, "token") // (Optional) Multi-Factor Authentication (MFA) token. This can also be sourced from the AWS_SESSION_TOKEN environment variable.
+	defaultStringField(l, "skip_region_validation")      // (Optional) Skip validation of provided region name.
+	defaultStringField(l, "skip_metadata_api_check")     // (Optional) Skip usage of EC2 Metadata API.
+	defaultStringField(l, "sts_endpoint")                // (Optional) Custom endpoint for the AWS Security Token Service (STS) API. This can also be sourced from the AWS_STS_ENDPOINT environment variable.
+	defaultStringField(l, "token")                       // (Optional) Multi-Factor Authentication (MFA) token. This can also be sourced from the AWS_SESSION_TOKEN environment variable.
 
-	defaultStringField(l, "assume_role_duration_seconds") // (Optional) Number of seconds to restrict the assume role session duration.
-	defaultStringField(l, "assume_role_policy") // (Optional) IAM Policy JSON describing further restricting permissions for the IAM Role being assumed.
-	defaultStringField(l, "assume_role_policy_arns") // (Optional) Set of Amazon Resource Names (ARNs) of IAM Policies describing further restricting permissions for the IAM Role being assumed.
-	defaultStringField(l, "assume_role_tags") // (Optional) Map of assume role session tags.
+	defaultStringField(l, "assume_role_duration_seconds")    // (Optional) Number of seconds to restrict the assume role session duration.
+	defaultStringField(l, "assume_role_policy")              // (Optional) IAM Policy JSON describing further restricting permissions for the IAM Role being assumed.
+	defaultStringField(l, "assume_role_policy_arns")         // (Optional) Set of Amazon Resource Names (ARNs) of IAM Policies describing further restricting permissions for the IAM Role being assumed.
+	defaultStringField(l, "assume_role_tags")                // (Optional) Map of assume role session tags.
 	defaultStringField(l, "assume_role_transitive_tag_keys") // (Optional) Set of assume role session tag keys to pass to any subsequent sessions.
-	defaultStringField(l, "external_id") // (Optional) External identifier to use when assuming the role.
-	defaultStringField(l, "role_arn") // (Optional) Amazon Resource Name (ARN) of the IAM Role to assume.
-	defaultStringField(l, "session_name") // (Optional) Session name to use when assuming the role.
-	
-	defaultStringField(l, "bucket") // (Required) Name of the S3 Bucket.
-	defaultStringField(l, "key") // (Required) Path to the state file inside the S3 Bucket. When using a non-default workspace, the state path will be /workspace_key_prefix/workspace_name/key (see also the workspace_key_prefix configuration).
+	defaultStringField(l, "external_id")                     // (Optional) External identifier to use when assuming the role.
+	defaultStringField(l, "role_arn")                        // (Optional) Amazon Resource Name (ARN) of the IAM Role to assume.
+	defaultStringField(l, "session_name")                    // (Optional) Session name to use when assuming the role.
 
-	defaultStringField(l, "acl") // (Optional) Canned ACL to be applied to the state file.
-	defaultStringField(l, "encrypt") // (Optional) Enable server side encryption of the state file.
-	defaultStringField(l, "endpoint") // (Optional) Custom endpoint for the AWS S3 API. This can also be sourced from the AWS_S3_ENDPOINT environment variable.
-	defaultStringField(l, "force_path_style") // (Optional) Enable path-style S3 URLs (https://<HOST>/<BUCKET> instead of https://<BUCKET>.<HOST>).
-	defaultStringField(l, "kms_key_id") // (Optional) Amazon Resource Name (ARN) of a Key Management Service (KMS) Key to use for encrypting the state.
-	defaultStringField(l, "sse_customer_key") // (Optional) The key to use for encrypting state with Server-Side Encryption with Customer-Provided Keys (SSE-C). This is the base64-encoded value of the key, which must decode to 256 bits. This can also be sourced from the AWS_SSE_CUSTOMER_KEY environment variable, which is recommended due to the sensitivity of the value. Setting it inside a terraform file will cause it to be persisted to disk in terraform.tfstate.
+	defaultStringField(l, "bucket") // (Required) Name of the S3 Bucket.
+	defaultStringField(l, "key")    // (Required) Path to the state file inside the S3 Bucket. When using a non-default workspace, the state path will be /workspace_key_prefix/workspace_name/key (see also the workspace_key_prefix configuration).
+
+	defaultStringField(l, "acl")                  // (Optional) Canned ACL to be applied to the state file.
+	defaultStringField(l, "encrypt")              // (Optional) Enable server side encryption of the state file.
+	defaultStringField(l, "endpoint")             // (Optional) Custom endpoint for the AWS S3 API. This can also be sourced from the AWS_S3_ENDPOINT environment variable.
+	defaultStringField(l, "force_path_style")     // (Optional) Enable path-style S3 URLs (https://<HOST>/<BUCKET> instead of https://<BUCKET>.<HOST>).
+	defaultStringField(l, "kms_key_id")           // (Optional) Amazon Resource Name (ARN) of a Key Management Service (KMS) Key to use for encrypting the state.
+	defaultStringField(l, "sse_customer_key")     // (Optional) The key to use for encrypting state with Server-Side Encryption with Customer-Provided Keys (SSE-C). This is the base64-encoded value of the key, which must decode to 256 bits. This can also be sourced from the AWS_SSE_CUSTOMER_KEY environment variable, which is recommended due to the sensitivity of the value. Setting it inside a terraform file will cause it to be persisted to disk in terraform.tfstate.
 	defaultStringField(l, "workspace_key_prefix") // (Optional) Prefix applied to the state path inside the bucket. This is only relevant when using a non-default workspace. Defaults to env:.
 
 	defaultStringField(l, "dynamodb_endpoint") // (Optional) Custom endpoint for the AWS DynamoDB API. This can also be sourced from the AWS_DYNAMODB_ENDPOINT environment variable.
-	defaultStringField(l, "dynamodb_table") // (Optional) Name of DynamoDB Table to use for state locking and consistency. The table must have a primary key named LockID with type of string. If not configured, state locking will be disabled.
+	defaultStringField(l, "dynamodb_table")    // (Optional) Name of DynamoDB Table to use for state locking and consistency. The table must have a primary key named LockID with type of string. If not configured, state locking will be disabled.
 	b.AddNestedMessage(l)
 	oof.AddChoice(builder.NewField("s3", builder.FieldTypeMessage(l)))
 	return nil
@@ -152,7 +152,7 @@ func addTerraformBackend(tf *builder.MessageBuilder) error {
 	oneof := builder.NewOneOf("config")
 	addTerraformLocalBackend(backend, oneof)
 	addTerraformS3Backend(backend, oneof)
-	
+
 	backend.AddOneOf(oneof)
 	tf.AddNestedMessage(backend)
 	tf.AddField(builder.NewField("backend", builder.FieldTypeMessage(backend)))
@@ -169,7 +169,7 @@ func addTerraformConfigMessage(main *builder.MessageBuilder) error {
 
 	local.AddField(fieldTerraformVersion)
 	local.AddNestedMessage(msgProvider)
-	local.AddField(builder.NewMapField("required_providers", builder.FieldTypeString(), builder.FieldTypeMessage(msgProvider)))
+	local.AddField(builder.NewMapField("required_providers", builder.FieldTypeString(), builder.FieldTypeMessage(msgProvider)).SetJsonName("required_providers"))
 	addTerraformBackend(local)
 
 	main.AddNestedMessage(local)
@@ -188,7 +188,6 @@ func (g *Generator) Save() error {
 	data := builder.NewMessage("Datasources")
 	providers := builder.NewMessage("Providers")
 
-
 	main.AddNestedMessage(resources)
 	main.AddField(builder.NewField("resource", builder.FieldTypeMessage(resources)).SetJsonName("resource"))
 	main.AddNestedMessage(data)
@@ -200,7 +199,6 @@ func (g *Generator) Save() error {
 	addLocalMessage(main)
 	addModuleMessage(main)
 	addTerraformConfigMessage(main)
-
 
 	metaFile := meta.MetaFile()
 	protoFiles := []*builder.FileBuilder{file, metaFile}
