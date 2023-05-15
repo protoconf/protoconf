7fc214141cfdccb00dcfe62a7ee49ab0eae6716e
Shahar Mintz <smintz@users.noreply.github.com>
2023-05-15 11:18:02 +0700

    chore: remove importers (#302)

--- cmd/protoconf/main.go
+++ cmd/protoconf/main.go
@@ -6,8 +6,6 @@ import (
 	"github.com/protoconf/protoconf/command"
 	"github.com/protoconf/protoconf/compiler"
 	"github.com/protoconf/protoconf/exec"
-	golangimporter "github.com/protoconf/protoconf/importers/golang_importer"
-	terraformimporter "github.com/protoconf/protoconf/importers/terraform_importer"
 	"github.com/protoconf/protoconf/inserter"
 	"github.com/protoconf/protoconf/mutate"
 	"github.com/protoconf/protoconf/server"
@@ -16,14 +14,12 @@ import (
 func main() {
 	command.RunSubcommands("protoconf",
 		map[string]cli.CommandFactory{
-			"agent":            agent.Command,
-			"compile":          compiler.Command,
-			"exec":             exec.Command,
-			"import golang":    golangimporter.Command,
-			"import terraform": terraformimporter.Command,
-			"insert":           inserter.Command,
-			"mutate":           mutate.Command,
-			"serve":            server.Command,
+			"agent":   agent.Command,
+			"compile": compiler.Command,
+			"exec":    exec.Command,
+			"insert":  inserter.Command,
+			"mutate":  mutate.Command,
+			"serve":   server.Command,
 		},
 	)
 }
