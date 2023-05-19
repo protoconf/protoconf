0405abaf623efd54900bdb7c2868a4038452bf2e
Shahar Mintz <smintz@protoconf.com>
2023-05-19 13:01:19 +0700

    fix: github actions push docker

--- website/protoconf/src/pages/index.tsx
+++ website/protoconf/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/0.1.7/intro">
             Getting Started
           </Link>
         </div>
