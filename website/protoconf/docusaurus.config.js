4e0d191879e970ba9a9c73c3224be5f67d8bf87d
Shahar Mintz <smintz@protoconf.com>
2023-05-19 13:11:17 +0700

    docs: fix footer link

--- website/protoconf/docusaurus.config.js
+++ website/protoconf/docusaurus.config.js
@@ -145,7 +145,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: 'docs/0.1.7/intro',
               },
             ],
           },
