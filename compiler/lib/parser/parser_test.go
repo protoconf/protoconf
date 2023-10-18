1ee0e0e8f3ef22f098db98f292ca1d5b2e350215
Shahar Mintz <smintz@protoconf.com>
2023-10-18 16:34:52 +0700

    test(compiler): add test cases to parser

---
+++ compiler/lib/parser/parser_test.go
@@ -0,0 +1,45 @@
+package parser
+
+import (
+	"testing"
+
+	_ "github.com/protoconf/proto-validate-reflect/validate"
+)
+
+func TestParser_ParseFilesX(t *testing.T) {
+	type fields struct {
+		protoconfRoot string
+	}
+	type args struct {
+		filenames []string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name:   "test",
+			fields: fields{"../testdata"},
+			args:   args{filenames: []string{"test.proto"}},
+		},
+		{
+			name:    "test not found",
+			fields:  fields{"../testdata"},
+			args:    args{filenames: []string{"test1.proto"}},
+			wantErr: true,
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := NewParser(tt.fields.protoconfRoot)
+			_, err := p.ParseFilesX(tt.args.filenames...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Parser.ParseFilesX() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
