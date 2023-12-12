a57c5c32189f24c1e678805cd8a86ee23e335909
Shahar Mintz <smintz@protoconf.com>
2023-12-13 00:02:37 +0700

    test(parser): ReadConfig

--- compiler/lib/parser/parser_test.go
+++ compiler/lib/parser/parser_test.go
@@ -7,8 +7,10 @@ import (
 
 	_ "github.com/protoconf/proto-validate-reflect/validate"
 	"github.com/protoconf/protoconf/consts"
+	v1 "github.com/protoconf/protoconf/datatypes/proto/v1"
 	"github.com/protoconf/protoconf/utils"
 	"github.com/protoconf/protoconf/utils/testdata"
+	"google.golang.org/protobuf/proto"
 )
 
 func TestParser_ParseFilesX(t *testing.T) {
@@ -46,6 +48,52 @@ func TestParser_ParseFilesX(t *testing.T) {
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Parser.ParseFilesX() error = %v, wantErr %v", err, tt.wantErr)
 				return
+			}
+		})
+	}
+}
+
+func TestParser_ReadConfig(t *testing.T) {
+	type fields struct {
+		protoconfRoot string
+	}
+	type args struct {
+		filename string
+		msg      proto.Message
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name:   "test",
+			fields: fields{filepath.Join(testdata.SmallTestDir())},
+			args: args{
+				filename: "materialized_config/test.materialized_JSON",
+				msg:      &v1.ProtoconfValue{},
+			},
+			wantErr: false,
+		},
+		{
+			name:   "test not found",
+			fields: fields{filepath.Join(testdata.SmallTestDir())},
+			args: args{
+				filename: "materialized_config/test.materialized_JSON1",
+				msg:      &v1.ProtoconfValue{},
+			},
+			wantErr: true,
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dr := utils.NewDescriptorRegistry()
+			dr.Import(utils.Parse, []*regexp.Regexp{}, tt.fields.protoconfRoot)
+			p := NewParser(dr.GetFilesResolver())
+			if err := p.ReadConfig(filepath.Join(tt.fields.protoconfRoot, tt.args.filename), tt.args.msg); (err != nil) != tt.wantErr {
+				t.Errorf("Parser.ReadConfig() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
